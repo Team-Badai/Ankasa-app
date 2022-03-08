f56724068ce2e5069759c95480a3578dcd7e6eb0
ferdif <ferdifh82@gmail.com>
2022-03-08 21:29:11 +0700

    fix conflicts in redux state

--- src/redux/store.js
+++
@@ -1,8 +0,0 @@
-import { createStore, applyMiddleware } from "redux"
-import logger from "redux-logger"
-import thunk from "redux-thunk"
-import rootReducers from "./reducers"
-
-let store = createStore(rootReducers, applyMiddleware(thunk, logger))
-
-export default store
