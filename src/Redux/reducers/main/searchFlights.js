7dc4e3a6d62d1410651d73f13626d4e6370d7084
ferdif <ferdifh82@gmail.com>
2022-03-09 01:02:31 +0700

    add redux state for Search Flight

---
+++ src/Redux/reducers/main/searchFlights.js
@@ -0,0 +1,20 @@
+const initialState = {
+  data: [],
+  loading: false,
+  error: false
+};
+
+const FetchSearchFlights = (state = initialState, action = {}) => {
+  switch (action.type) {
+    case "SEARCH_FLIGHTS_REQUEST":
+      return { ...state, loading: true };
+    case "SEARCH_FLIGHTS_SUCCESS":
+      return { ...state, loading: false, data: action.payload };
+    case "SEARCH_FLIGHTS_FAIL":
+      return { ...state, loading: false, error: action.payload };
+    default:
+      return state;
+  }
+};
+
+export default FetchSearchFlights;
