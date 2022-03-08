bda977e70029427c0a994c76ee0db28b739af887
yuliamassel <yuliamassel8@gmail.com>
2022-03-08 06:52:54 -0800

    fixed page auth

--- src/Pages/Auth/SignUp/index.js
+++ src/Pages/Auth/SignUp/index.js
@@ -5,6 +5,8 @@ import Logo from "../../../assets/img/Logo2.svg";
 import Button from "../../../Components/Button";
 import Input from "../../../Components/Input";
 import "../auth.css";
+import { useDispatch } from "react-redux";
+import { AuthSignUp } from "../../../Redux/actions/Auth/authSignUp";
 
 const SignUp = () => {
   const navigate = useNavigate();
@@ -13,7 +15,22 @@ const SignUp = () => {
     fullname: "",
     email: "",
     password: "",
+    requisite: "",
   });
+
+  const FormAddUser = new FormData();
+  FormAddUser.append("fullname", form.fullname);
+  FormAddUser.append("email", form.email);
+  FormAddUser.append("password", form.password);
+  FormAddUser.append("equisite", form.requisite);
+
+  const dispatch = useDispatch();
+  const [checked, setChecked] = useState("");
+
+  const handleCheckbox = (e) => {
+    console.log("CHECKED",e.target.value);
+    setChecked(e.target.value)
+  };
 
   const handleChange = (e) => {
     setFrom({
@@ -32,6 +49,12 @@ const SignUp = () => {
     navigate("/auth/login");
   };
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(AuthSignUp({ form }));
+    console.log("ISI FORM",form);
+  };
+
   return (
     <Fragment>
       <section className="row col-xl-4 right-section p-xl-5">
@@ -41,47 +64,59 @@ const SignUp = () => {
           </div>
           <div className="content">
             <p className="title ms-xl-5">Register</p>
-            <Input
-              className="input-login"
-              placeholder="Fullname"
-              type="text"
-              name="fullname"
-              onChange={handleChange}
-              value={form.fullname}
-            />
-            <Input
-              className="input-login pt-4"
-              placeholder="Email"
-              type="email"
-              name="email"
-              onChange={handleChange}
-              value={form.email}
-            />
-            <div>
+            <form onSubmit={handleSubmit}>
+              <Input
+                className="input-login"
+                placeholder="Fullname"
+                type="text"
+                name="fullname"
+                onChange={handleChange}
+                value={form.fullname}
+              />
               <Input
                 className="input-login pt-4"
-                placeholder="Password"
-                type={showPassword ? "text" : "password"}
-                name="password"
+                placeholder="Email"
+                type="email"
+                name="email"
                 onChange={handleChange}
-                value={form.password}
+                value={form.email}
               />
-              {showPassword ? (
-                < BsIcons.BsEye
-                  onClick={handleShowPassword}
-                  className="form-icons bi-eye-slash position-absolute"
+              <div>
+                <Input
+                  className="input-login pt-4"
+                  placeholder="Password"
+                  type={showPassword ? "text" : "password"}
+                  name="password"
+                  onChange={handleChange}
+                  value={form.password}
                 />
-              ) : (
-                <BsIcons.BsEyeSlash
-                  onClick={handleShowPassword}
-                  className="form-icons bi-eye-slash position-absolute"
+                {showPassword ? (
+                  <BsIcons.BsEye
+                    onClick={handleShowPassword}
+                    className="form-icons bi-eye-slash position-absolute mt-4"
+                  />
+                ) : (
+                  <BsIcons.BsEyeSlash
+                    onClick={handleShowPassword}
+                    className="form-icons bi-eye-slash position-absolute mt-4"
+                  />
+                )}
+              </div>
+              <Button className="btn-login mt-5" type="submit">
+                Sign Up
+              </Button>
+            </form>
+            <div className="bot-action my-3">                
+            <Input
+                  onChange={handleCheckbox}
+                  value='Accept terms and condition'
+                  name="checked"
+                  className="input-check me-2 mt-2"
+                  checked={checked === "Accept terms and condition"}
+                  type="checkbox"
+                  id="checked"
                 />
-              )}
-            </div>
-            <Button className="btn-login mt-5">Sign Up</Button>
-            <div className="bot-action my-3">
-              <Input className="input-check me-2 mt-2" type="checkbox" id="check" />
-              <label>Accept terms and condition</label>
+                <label>Accept terms and condition</label>
             </div>
             <hr size="4" />
             <p>Already have an account?</p>
